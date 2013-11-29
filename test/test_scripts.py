a43172953a7a488acce81e9c7442085602c764a9
Tim Cuthbertson <tim@gfxmonk.net>
2013-11-29 14:51:35 +1100

    fix test_interpreter test

--- test/test_scripts.py
+++ test/test_scripts.py
@@ -4,7 +4,7 @@ class TestScripts(TestCase):
 	@unittest.skipIf(IS_WINDOWS, 'posix')
 	def test_interpreter(self):
 		self.write('gup/all.gup', '#!./build abc\n# ...')
-		self.write('gup/build', BASH + '(echo "target: $4"; echo "arg: $1") > "$3"')
+		self.write('gup/build', '#!/bin/bash\nset -eu\n' + '(echo "target: $4"; echo "arg: $1") > "$3"')
 		os.chmod(self.path('gup/build'), 0755)
 
 		self.build('all')
