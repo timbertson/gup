02c92b1efabc8ced09b72cd70d716b84836604e7
Tim Cuthbertson <tim@gfxmonk.net>
2013-12-29 15:14:09 +1100

    typo

--- python/gup/cmd.py
+++ python/gup/cmd.py
@@ -159,7 +159,7 @@ def _mark_ifcreate(opts, files):
 		parent_state.add_dependency(FileDependency.relative_to_target(parent_target, mtime=None, checksum=None, path = filename))
 
 def _mark_contents(opts, targets):
-	parent_target = _assert_parent_target('--content')
+	parent_target = _assert_parent_target('--contents')
 	if len(targets) == 0:
 		assert not sys.stdin.isatty()
 		checksum = Checksum.from_stream(sys.stdin)
