ed27dc13a6de940358ef1ccf014fb66becb6e90e
Tim Cuthbertson <tim@gfxmonk.net>
2015-08-07 20:40:48 +1000

    fish completions: ignore stderr

--- share/fish/completions/gup.fish
+++ share/fish/completions/gup.fish
@@ -1,2 +1,2 @@
 complete -e -c gup
-complete -c gup --no-files --arguments '(gup --complete-command (commandline --current-token))'
+complete -c gup --no-files --arguments '(gup --complete-command (commandline --current-token) 2>/dev/null)'
