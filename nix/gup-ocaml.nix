58adf92773c879a9d9f98741060acb357a169671
Tim Cuthbertson <tim@gfxmonk.net>
2015-09-07 19:05:51 +1000

    soft tabs in nix/

--- nix/gup-ocaml.nix
+++ nix/gup-ocaml.nix
@@ -1,33 +1,33 @@
 { stdenv, lib, fetchurl, python, which, zlib,
-	ocaml, findlib,
-	ocaml_batteries, cryptokit, extunix, ocaml_lwt
+  ocaml, findlib,
+  ocaml_batteries, cryptokit, extunix, ocaml_lwt
 }:
 { src, version, meta ? {} }:
 let
-	ocaml_version = (builtins.parseDrvName ocaml.name).version;
+  ocaml_version = (builtins.parseDrvName ocaml.name).version;
 
-	# required only for development (.byte targets)
-	libdirs = map ({dep, name}: "${dep}/lib/ocaml/${ocaml_version}/site-lib/${name}") [
-		{dep = ocaml_lwt; name = "lwt";}
-		{dep = cryptokit; name = "cryptokit";}
-		{dep = extunix; name = "extunix";}
-	];
-	add_ldpath = ''
-		export LD_LIBRARY_PATH="''${LD_LIBRARY_PATH:+:}${lib.concatStringsSep ":" libdirs}";
-	'';
+  # required only for development (.byte targets)
+  libdirs = map ({dep, name}: "${dep}/lib/ocaml/${ocaml_version}/site-lib/${name}") [
+    {dep = ocaml_lwt; name = "lwt";}
+    {dep = cryptokit; name = "cryptokit";}
+    {dep = extunix; name = "extunix";}
+  ];
+  add_ldpath = ''
+    export LD_LIBRARY_PATH="''${LD_LIBRARY_PATH:+:}${lib.concatStringsSep ":" libdirs}";
+  '';
 in
 stdenv.mkDerivation {
-	name = "gup-${version}";
-	inherit src meta;
-	buildInputs = [
-		python which zlib
-		ocaml findlib ocaml_batteries cryptokit extunix ocaml_lwt
-	];
-	shellHook = add_ldpath;
-	buildPhase = "make -C ocaml native";
-	installPhase = ''
-		mkdir $out
-		cp -r ocaml/bin $out/bin
-		cp -r share $out/share
-	'';
+  name = "gup-${version}";
+  inherit src meta;
+  buildInputs = [
+    python which zlib
+    ocaml findlib ocaml_batteries cryptokit extunix ocaml_lwt
+  ];
+  shellHook = add_ldpath;
+  buildPhase = "make -C ocaml native";
+  installPhase = ''
+    mkdir $out
+    cp -r ocaml/bin $out/bin
+    cp -r share $out/share
+  '';
 }
--- nix/gup-python.nix
+++ nix/gup-python.nix
@@ -1,13 +1,13 @@
 { stdenv, lib, python, which, pychecker ? null }:
 { src, version, meta ? {} }:
 stdenv.mkDerivation {
-	inherit src meta;
-	name = "gup-${version}";
-	buildInputs = lib.remove null [ python which pychecker ];
-	SKIP_PYCHECKER = pychecker == null;
-	buildPhase = "make python";
-	installPhase = ''
-		mkdir $out
-		cp -r python/bin $out/bin
-	'';
+  inherit src meta;
+  name = "gup-${version}";
+  buildInputs = lib.remove null [ python which pychecker ];
+  SKIP_PYCHECKER = pychecker == null;
+  buildPhase = "make python";
+  installPhase = ''
+    mkdir $out
+    cp -r python/bin $out/bin
+  '';
 }
