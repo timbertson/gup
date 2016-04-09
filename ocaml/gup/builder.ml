timbertson/gup

commit 2aa2f41c22be27436a821935d0317a81c7106c29

delay buildscript symlink resolution until calculating dependency information
.. so that symlinked buildscripts are invoked via the link.
Some build scripts need to know _which_ path they were invoked by,
and pre-resolving symlinks breaks that.

ocaml/gup/gupfile.ml
@@ -193,7 +193,6 @@ class buildscript
 	(script_path:string)
 	(target:string)
 	(basedir:string) =
-	let script_path = Util.realpath script_path in
 	object (self)
 		method repr = Printf.sprintf "buildscript(%s, %s, %s)" script_path target basedir
 		method target = target

ocaml/gup/state.ml
@@ -462,6 +462,15 @@ and target_state (target_path:string) =
 		method mark_always_rebuild =
 			self#add_dependency (serializable (new always_rebuild))
 
+		method private builder_dependency path =
+			let path = Util.realpath path in
+			lwt builder_mtime = Util.get_mtime path in
+			return (new builder_dependency
+				~mtime: builder_mtime
+				~checksum: None
+				(Util.relpath ~from: (Filename.dirname self#path) path)
+			)
+
 		method perform_build exe block : bool Lwt.t =
 			assert (Sys.file_exists exe);
 			log#trace "perform_build %s" self#path;
@@ -478,13 +487,7 @@ and target_state (target_path:string) =
 				if (!builds_have_been_cancelled) then raise Error.BuildCancelled;
 				lwt deps = self#deps in
 				if still_needs_build deps then (
-					lwt builder_mtime = Util.get_mtime exe in
-					let builder_dep = new builder_dependency
-						~mtime: builder_mtime
-						~checksum: None
-						(Util.relpath ~from: (Filename.dirname self#path) exe)
-					in
-
+					lwt builder_dep = self#builder_dependency exe in
 					let temp = ensure_meta_path new_deps_ext in
 					with_file_out temp (fun file ->
 						Lwt_io.write_line file (version_marker ^ (string_of_int format_version)) >>

python/gup/gupfile.py
@@ -132,7 +132,7 @@ class Builder(object):
 	builder was obtained indirectly (via a Gupfile match)
 	'''
 	def __init__(self, script_path, target, basedir):
-		self.path = os.path.realpath(script_path)
+		self.path = script_path
 		self.target = target
 		self.basedir = basedir
 		self.target_path = os.path.join(self.basedir, self.target)

python/gup/state.py
@@ -99,9 +99,10 @@ class TargetState(object):
 			if not still_needs_build(deps):
 				return False
 
+			builder_path = os.path.realpath(exe)
 			builder_dep = BuilderDependency.relative_to_target(self.path,
-				path=exe,
-				mtime=get_mtime(exe))
+				path=builder_path,
+				mtime=get_mtime(builder_path))
 
 			_log.trace("created dep %s from builder %r" % (builder_dep, exe))
 			temp = self._ensure_meta_path('deps2')

test/test_scripts.py
@@ -241,3 +241,7 @@ class TestSymlinkScripts(TestCase):
 		self.assertTrue(os.path.islink(self.path('link')))
 		self.assertEquals(self._output_files(), [])
 
+	def test_builder_is_invoked_via_link(self):
+		self.write('buildscript.sh', echo_to_target('$(basename "$0")'))
+		self.symlink('buildscript.sh', 'target.gup')
+		self.build_assert('target', 'target.gup')

commit bb6249c9e993a18b9c9e3356ee2896e6fed90153

fix regression introduced in 2aa2f41c22be27436a821935d0317a81c7106c29 where targets built by a symlinked builder are always rebuilt

ocaml/gup/builder.ml
@@ -117,9 +117,9 @@ class target (buildscript:Gupfile.buildscript) =
 		method repr = "Target(" ^ buildscript#repr ^ ")"
 		method state = state
 
-		method build update : bool Lwt.t = self#_perform_build buildscript#path update
+		method build update : bool Lwt.t = self#_perform_build update
 
-		method private _perform_build (exe_path:string) (update: bool) : bool Lwt.t =
+		method private _perform_build (update: bool) : bool Lwt.t =
 			let exe_path = Util.abspath buildscript#path in
 			if not (Sys.file_exists exe_path) then
 				Error.raise_safe "Build script does not exist: %s" exe_path;
@@ -129,7 +129,7 @@ class target (buildscript:Gupfile.buildscript) =
 				log#trace("no build needed");
 				Lwt.return false
 			) else (
-				state#perform_build exe_path (fun exe deps ->
+				state#perform_build buildscript (fun exe deps ->
 
 					let basedir = buildscript#basedir in
 					Util.makedirs basedir;

ocaml/gup/gupfile.ml
@@ -193,11 +193,13 @@ class buildscript
 	(script_path:string)
 	(target:string)
 	(basedir:string) =
+		let realpath = Util.realpath script_path in
 	object (self)
 		method repr = Printf.sprintf "buildscript(%s, %s, %s)" script_path target basedir
 		method target = target
 		method target_path = Filename.concat basedir target
 		method path = script_path
+		method realpath = realpath
 		method basedir = basedir
 	end
 

ocaml/gup/state.ml
@@ -277,7 +277,7 @@ and dependencies target_path (data:base_dependency intermediate_dependencies) =
 				let args = {
 					path = target_path;
 					base_path = base_path;
-					builder_path = Util.relpath ~from:base_path buildscript#path;
+					builder_path = Util.relpath ~from:base_path buildscript#realpath;
 					built = built
 				} in
 
@@ -463,7 +463,6 @@ and target_state (target_path:string) =
 			self#add_dependency (serializable (new always_rebuild))
 
 		method private builder_dependency path =
-			let path = Util.realpath path in
 			lwt builder_mtime = Util.get_mtime path in
 			return (new builder_dependency
 				~mtime: builder_mtime
@@ -471,7 +470,8 @@ and target_state (target_path:string) =
 				(Util.relpath ~from: (Filename.dirname self#path) path)
 			)
 
-		method perform_build exe block : bool Lwt.t =
+		method perform_build (buildscript:Gupfile.buildscript) block : bool Lwt.t =
+			let exe = buildscript#path in
 			assert (Sys.file_exists exe);
 			log#trace "perform_build %s" self#path;
 
@@ -487,7 +487,7 @@ and target_state (target_path:string) =
 				if (!builds_have_been_cancelled) then raise Error.BuildCancelled;
 				lwt deps = self#deps in
 				if still_needs_build deps then (
-					lwt builder_dep = self#builder_dependency exe in
+					lwt builder_dep = self#builder_dependency buildscript#realpath in
 					let temp = ensure_meta_path new_deps_ext in
 					with_file_out temp (fun file ->
 						Lwt_io.write_line file (version_marker ^ (string_of_int format_version)) >>

ocaml/gup/state.mli
@@ -19,7 +19,7 @@ class target_state : string ->
 		method path : string
 
 		(* async methods *)
-		method perform_build : string -> (string -> dependencies option -> bool Lwt.t) -> bool Lwt.t
+		method perform_build : Gupfile.buildscript -> (string -> dependencies option -> bool Lwt.t) -> bool Lwt.t
 		method deps : dependencies option Lwt.t
 		method add_file_dependency : mtime:(Big_int.t option) -> checksum:(string option) -> string -> unit Lwt.t
 		method add_checksum : string -> unit Lwt.t

python/gup/builder.py
@@ -85,7 +85,7 @@ class Target(object):
 		return 'Target(%r)' % (self.path,)
 	
 	def build(self, update):
-		return self.state.perform_build(self.builder.path, lambda deps: self._perform_build(update, deps))
+		return self.state.perform_build(self.builder, lambda deps: self._perform_build(update, deps))
 
 	def _perform_build(self, update, deps):
 		'''
@@ -97,7 +97,7 @@ class Target(object):
 			if not _is_dirty(self.state):
 				_log.trace("no build needed")
 				return False
-		exe_path = os.path.abspath(self.builder.path)
+		exe_path = path.abspath(self.builder.path)
 		exe_path_relative_to_cwd = os.path.relpath(exe_path,ROOT_CWD)
 
 		# dest may not exist, if a /gup/ directory is in use

python/gup/gupfile.py
@@ -133,6 +133,7 @@ class Builder(object):
 	'''
 	def __init__(self, script_path, target, basedir):
 		self.path = script_path
+		self.realpath = os.path.realpath(self.path)
 		self.target = target
 		self.basedir = basedir
 		self.target_path = os.path.join(self.basedir, self.target)

python/gup/state.py
@@ -88,7 +88,8 @@ class TargetState(object):
 	def mark_clobbers(self):
 		self.add_dependency(ClobbersTarget())
 	
-	def perform_build(self, exe, do_build):
+	def perform_build(self, builder, do_build):
+		exe = builder.path
 		assert os.path.exists(exe)
 		def still_needs_build(deps):
 			_log.trace("checking if %s still needs build after releasing lock" % self.path)
@@ -99,10 +100,9 @@ class TargetState(object):
 			if not still_needs_build(deps):
 				return False
 
-			builder_path = os.path.realpath(exe)
 			builder_dep = BuilderDependency.relative_to_target(self.path,
-				path=builder_path,
-				mtime=get_mtime(builder_path))
+				path=builder.realpath,
+				mtime=get_mtime(builder.realpath))
 
 			_log.trace("created dep %s from builder %r" % (builder_dep, exe))
 			temp = self._ensure_meta_path('deps2')
@@ -165,7 +165,7 @@ class Dependencies(object):
 			return True
 
 		base = os.path.dirname(self.path)
-		builder_path = os.path.relpath(builder.path, base)
+		builder_path = os.path.relpath(builder.realpath, base)
 
 		unknown_states = []
 		dirty_args = _dirty_args(deps=self, base=base, builder_path=builder_path, built=built)