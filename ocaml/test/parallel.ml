a2ccb8f5d108d796250d6330ef4aa5f11cbff498
Tim Cuthbertson <tim@gfxmonk.net>
2017-01-01 22:34:59 +1100

    deal with make renaming jobserver-fds -> jobserver-auth

--- ocaml/test/parallel.ml
+++ ocaml/test/parallel.ml
@@ -11,8 +11,13 @@ let assertFds expected str =
 
 let suite = "MAKEFLAGS" >:::
 [
-	"extracts FDs" >:: (fun _ ->
+	"extracts FDs from --jobserver-fds" >:: (fun _ ->
 		assertFds (Some (1,2)) "--jobserver-fds=1,2";
 		assertFds (Some (100,200)) "make --jobserver-fds=100,200 -j";
-	)
+	);
+
+	"extracts FDs from --jobserver-auth" >:: (fun _ ->
+		assertFds (Some (1,2)) "--jobserver-auth=1,2";
+		assertFds (Some (100,200)) "make --jobserver-auth=100,200 -j";
+	);
 ]
